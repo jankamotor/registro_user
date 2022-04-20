d7f4338fb4115ebdcccdc6999909f36cf380e79b
jankamotor <juancarlos.rivas.alcon@ucreativa.com>
2022-04-19 18:33:40 -0600

    modificaion db.php #3

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 // Enter your Host, username, password, database below.
 // Set password on localhost.
 //$con = mysqli_connect("localhost","root","","register");
-$con = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
+$con = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
 // Check Check
 // Check connection
 if (mysqli_connect_errno())
