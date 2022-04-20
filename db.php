8ca49b2d0dabb5c2efd8a1dbb31eea2a465b7e2e
jankamotor <juancarlos.rivas.alcon@ucreativa.com>
2022-04-19 18:09:37 -0600

    Modificacion archivo dp.php bd-connection

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 // Set password on localhost.
 //$con = mysqli_connect("localhost","root","","register");
 $con = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
-
+// Check Check
 // Check connection
 if (mysqli_connect_errno())
   {
